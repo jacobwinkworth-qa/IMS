e39b38472c03a25d84592eebe0e596f0c6ca53e6
Jacob Winkworth <jwinkworth@academytrainee.com>
2020-11-17 17:24:46 +0000

    sql-schema update

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,5 @@
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO customers (first_name, surname) 
+VALUES ('Jacob', 'Winkworth');
+
+INSERT INTO items (name)
+VALUES ('Skateboard'); 
--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,12 +1,34 @@
 drop schema ims;
 
-CREATE SCHEMA IF NOT EXISTS `ims`;
+CREATE SCHEMA IF NOT EXISTS ims;
 
-USE `ims` ;
+USE ims;
 
-CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id INT(10) NOT NULL AUTO_INCREMENT,
+    first_names VARCHAR(40) DEFAULT NULL,
+    surname VARCHAR(40) DEFAULT NULL,
+    PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+	order_id INT(10) NOT NULL AUTO_INCREMENT,
+	customer_id INT(10) NOT NULL,
+	PRIMARY KEY (order_id) REFERENCES orders(order_id),
+	FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS items (
+	item_id INT(10) NOT NULL AUTO_INCREMENT,
+	name VARCHAR(40) NOT NULL,
+	value DOUBLE(6,2) NOT NULL
+	PRIMARY KEY (item_id)
+);	
+
+CREATE TABLE IF NOT EXISTS orders_items (
+	order_id INT(10) NOT NULL,
+	item_id INT(10) NOT NULL,
+	quantity INT(10) NOT NULL,
+	FOREIGN KEY (order_id) REFERENCES orders(order_id),
+	FOREIGN KEY (item_id) REFERENCES items(item_id)
 );
